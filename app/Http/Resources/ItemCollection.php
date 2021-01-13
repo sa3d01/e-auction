2accba35ce862c925b3e5688e760fd4e308bc4f3
sa3d01 <sa3dsalem01@gmail.com>
2021-01-13 17:18:33 +0200

    .

--- app/Http/Resources/ItemCollection.php
+++ app/Http/Resources/ItemCollection.php
@@ -36,14 +36,14 @@ class ItemCollection extends ResourceCollection
                 }
             }
             if ($auction_item){
-                if (Carbon::createFromTimestamp($auction_item->start_date)->addSeconds($auction_item->auction->duration) < Carbon::now(app()->get('timezone'))){
+                if (Carbon::createFromTimestamp($auction_item->start_date)->addSeconds($auction_item->auction->duration) < Carbon::now(app()->get('tz'))){
                     $arr['auction_status']='expired';
                     $auction_item->update([
                         'more_details'=>[
                             'status'=>'expired'
                         ]
                     ]);
-                }elseif ((Carbon::createFromTimestamp($auction_item->start_date) <= Carbon::now(app()->get('timezone')) )  &&  (Carbon::createFromTimestamp($auction_item->start_date)->addSeconds($auction_item->auction->duration) >= Carbon::now(app()->get('timezone')))){
+                }elseif ((Carbon::createFromTimestamp($auction_item->start_date) <= Carbon::now(app()->get('tz')) )  &&  (Carbon::createFromTimestamp($auction_item->start_date)->addSeconds($auction_item->auction->duration) >= Carbon::now(app()->get('tz')))){
                     $arr['auction_status']='live';
                 }else{
                     $arr['auction_status']='soon';
@@ -51,7 +51,7 @@ class ItemCollection extends ResourceCollection
                 $arr['auction_type']= $obj->auction_type->name[$this->lang()];
                 $arr['start_date']= $auction_item->start_date;
                 $arr['start_date_text']= Carbon::createFromTimestamp($auction_item->start_date)->format('Y-m-d H:i');
-                $arr['now_date']= Carbon::now(app()->get('timezone'))->format('Y-m-d H:i');
+                $arr['now_date']= Carbon::now(app()->get('tz'))->format('Y-m-d H:i');
                 $arr['auction_duration']=$auction_item->auction->duration;
                 $arr['auction_price']=$auction_item->price;
             }
