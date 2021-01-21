e9e2d6f83f3ee783a01b9d1363a4296333673503
sa3d01 <sa3dsalem01@gmail.com>
2021-01-22 01:30:58 +0200

    ..

--- app/AuctionItem.php
+++ app/AuctionItem.php
@@ -40,7 +40,7 @@ class AuctionItem extends Model
             if ($this->item->auction_type_id == 4) {
                 //البيع المباشر
                 $arr['user_price'] = $this->item->price;
-                if (Carbon::createFromTimestamp($this->start_date) < Carbon::now()) {
+                if ($this->more_details['status']=='soon') {
                     $arr['negotiation'] = true;
                     $arr['direct_pay'] = true;
                 }
