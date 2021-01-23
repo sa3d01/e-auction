c42db97a3d3abd55400055ddb0365066ac181068
sa3d01 <sa3dsalem01@gmail.com>
2021-01-23 03:48:11 +0200

    .

--- app/Observers/ItemObserver.php
+++ app/Observers/ItemObserver.php
@@ -13,11 +13,13 @@ class ItemObserver
     public function deleting(Item $item)
     {
         $auction_item = AuctionItem::where('item_id', $item->id)->first();
-        $offers = Offer::where('auction_item_id', $auction_item->id)->get();
-        foreach ($offers as $offer) {
-            $offer->delete();
+        if ($auction_item){
+            $offers = Offer::where('auction_item_id', $auction_item->id)->get();
+            foreach ($offers as $offer) {
+                $offer->delete();
+            }
+            $auction_item->delete();
         }
-        $auction_item->delete();
         $auction_users = AuctionUser::where('item_id', $item->id)->get();
         foreach ($auction_users as $auction_user) {
             $auction_user->delete();
