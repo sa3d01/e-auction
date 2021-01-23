b1ee6d11f0c045c6aff99dc699241df9ce67f5e0
sa3d01 <sa3dsalem01@gmail.com>
2021-01-23 15:36:59 +0200

    .

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -4,6 +4,8 @@ namespace App\Observers;
 
 use App\AuctionItem;
 use App\AuctionUser;
+use App\Contact;
+use App\FeedBack;
 use App\Item;
 use App\Notification;
 use App\Offer;
@@ -13,6 +15,18 @@ class UserObserver
 {
     public function deleting(User $user)
     {
+        $contacts=Contact::where('user_id',$user->id)->get();
+        foreach ($contacts as $contact){
+            $contact->delete();
+        }
+        $feed_backs=FeedBack::where('user_id',$user->id)->get();
+        foreach ($feed_backs as $feed_back){
+            $feed_back->delete();
+        }
+        $notifications = Notification::where('receiver_id', $user->id)->get();
+        foreach ($notifications as $notification) {
+            $notification->delete();
+        }
         $items=Item::where('user_id',$user->id)->get();
         foreach ($items as $item) {
             $auction_item = AuctionItem::where('item_id', $item->id)->first();
@@ -24,10 +38,6 @@ class UserObserver
             $auction_users = AuctionUser::where('item_id', $item->id)->get();
             foreach ($auction_users as $auction_user) {
                 $auction_user->delete();
-            }
-            $notifications = Notification::where('receiver_id', $user->id)->get();
-            foreach ($notifications as $notification) {
-                $notification->delete();
             }
             $item->delete();
         }
