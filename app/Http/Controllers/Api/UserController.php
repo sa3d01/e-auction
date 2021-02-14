4e99c6a23d76a4de1065da5a498b8597b6a61926
sa3d01 <sa3dsalem01@gmail.com>
2021-02-14 19:07:17 +0200

    .

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -190,12 +190,8 @@ class UserController extends MasterController
 
     public function myPaidItems(){
         $user = auth()->user();
-        $paid_auction_items=AuctionItem::where('more_details->status','paid')->get();
-        $item_ids_query = AuctionUser::where('user_id', $user->id);
-        foreach ($paid_auction_items as $paid_auction_item){
-            $item_ids_query = $item_ids_query->where('charge_price',$paid_auction_item->latest_charge);
-        }
-        $item_ids=$item_ids_query->pluck('item_id');
+        $paid_auction_items_latest_charge=AuctionItem::where('more_details->status','paid')->pluck('latest_charge');
+        $item_ids = AuctionUser::where('user_id', $user->id)->whereIn('charge_price',$paid_auction_items_latest_charge)->pluck('item_id');
         return $this->sendResponse(new ItemCollection(Item::whereIn('id',$item_ids)->latest()->get()));
     }
 
