c1eb2eb00d3617b21ac4e2f10b1169f5f58b299b
sa3d01 <sa3dsalem01@gmail.com>
2021-02-15 02:10:06 +0200

    .

--- app/Http/Controllers/Api/AuctionController.php
+++ app/Http/Controllers/Api/AuctionController.php
@@ -27,7 +27,7 @@ class AuctionController extends MasterController
     public function index(){
         $vip_items=AuctionItem::where('vip','true')->pluck('item_id');
         $data['vip']=new ItemCollection(Item::whereIn('id',$vip_items)->latest()->get());
-        $data['data']=new ItemCollection(Item::where('status','shown')->orWhere('status','expired')->latest()->get());
+        $data['data']=new ItemCollection(Item::where('status','shown')->orWhere('status','sold')->latest()->get());
         return $this->sendResponse($data);
     }
     public function auctions(){
