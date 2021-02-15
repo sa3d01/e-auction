ab0ef3f3450760d5e09fb05e57a59f581029cd98
sa3d01 <sa3dsalem01@gmail.com>
2021-02-15 03:12:25 +0200

    .

--- resources/views/dashboard/drop_down/create.blade.php
+++ resources/views/dashboard/drop_down/create.blade.php
@@ -40,22 +40,20 @@
                                 <fieldset class="form-group">
                                     <div class="row">
                                         @foreach($create_fields as $key=>$value)
-                                            @if($value=='note')
-                                                <div class="col-sm-12">
-                                                    <div class="form-group" id="{{$value}}">
-                                                        <label> {{$key}} </label>
-                                                        <textarea name="{{$value}}" class="form-control" cols="80" rows="5"></textarea>
-                                                    </div>
+                                            <div class="col-sm-12">
+                                                <div class="form-group" id="name_ar">
+                                                    <label for=""> الاسم باللغة العربية</label>
+                                                    <input name="name_ar" class="form-control" type="text">
+                                                    <div class="help-block form-text with-errors form-control-feedback"></div>
                                                 </div>
-                                            @else
-                                                <div class="col-sm-12">
-                                                    <div class="form-group" id="{{$value}}">
-                                                        <label for=""> {{$key}}</label>
-                                                        <input name="{{$value}}" class="form-control" type="text">
-                                                        <div class="help-block form-text with-errors form-control-feedback"></div>
-                                                    </div>
+                                            </div>
+                                            <div class="col-sm-12">
+                                                <div class="form-group" id="name_en">
+                                                    <label for=""> الاسم باللغة الانجليزية</label>
+                                                    <input name="name_en" class="form-control" type="text">
+                                                    <div class="help-block form-text with-errors form-control-feedback"></div>
                                                 </div>
-                                            @endif
+                                            </div>
                                         @endforeach
                                             @if(isset($image))
                                                 <div class="col-sm-12">
