87491fbb2e85ce6f5c51565f30447e065acbc97d
sa3d01 <sa3dsalem01@gmail.com>
2021-03-30 22:54:19 +0200

    dashboard

--- resources/views/dashboard/item/show.blade.php
+++ resources/views/dashboard/item/show.blade.php
@@ -205,14 +205,14 @@
                                                     </div>
                                                 </div>
                                             @endif
-                                                <div class="col-sm-12">
-                                                    <div class="form-group" id="shipping_by" data-value="{{$row->shipping_by}}">
-                                                        <label for=""> نوع الشحن</label>
-                                                        <input disabled name="shipping_by" class="form-control" value="{{$row->shipping_by}}" type="text">
-                                                        <div class="help-block form-text with-errors form-control-feedback"></div>
-                                                    </div>
-                                                </div>
                                         @endforeach
+                                            <div class="col-sm-12">
+                                                <div class="form-group" id="shipping_by" data-value="{{$row->shipping_by}}">
+                                                    <label for=""> نوع الشحن</label>
+                                                    <input disabled name="shipping_by" class="form-control" value="{{$row->shipping_by}}" type="text">
+                                                    <div class="help-block form-text with-errors form-control-feedback"></div>
+                                                </div>
+                                            </div>
                                             @if(isset($selects))
                                                 @foreach($selects as $select)
                                                     @php($related_model=$select['name'])
