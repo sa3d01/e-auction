0758d1607d29b7d7c8e1a178f8b20fa721eb7495
sa3d01 <sa3dsalem01@gmail.com>
2021-03-31 00:21:45 +0200

    dashboard

--- app/Http/Controllers/Admin/ItemController.php
+++ app/Http/Controllers/Admin/ItemController.php
@@ -62,7 +62,7 @@ class ItemController extends MasterController
             foreach ($request->input('images') as $image){
                 $filename=null;
                 if (is_file($image)) {
-                    if ($image->getSize() > 10000000000){
+                    if ($image->getSize() > 1000000){
                         return redirect()->back()->withErrors(['msg', 'حجم الصورة كبير جدا..']);
                     }
                     $filename = Str::random(10) . '.' . $image->getClientOriginalExtension();
@@ -158,7 +158,7 @@ class ItemController extends MasterController
             foreach ($request->input('images') as $image){
                 $filename=null;
                 if (is_file($image)) {
-                    if ($image->getSize()  > 10000000000){
+                    if ($image->getSize()  > 1000000){
                         return redirect()->back()->withErrors(['msg', 'حجم الصورة كبير جدا..']);
                     }
                     $filename = Str::random(10) . '.' . $image->getClientOriginalExtension();
