440a8f9e5e6a1ea5fac40003db93ef948c3d0ff8
sa3d01 <sa3dsalem01@gmail.com>
2021-04-13 23:34:57 +0200

    api

--- app/AuctionItem.php
+++ app/AuctionItem.php
@@ -37,7 +37,9 @@ class AuctionItem extends Model
             $arr['user_price'] = $this->item->price;
         }
         if ($this->more_details['status']!='paid' && $this->more_details['status']!='expired' && $this->more_details['status']!='negotiation' && $this->more_details['status']!='delivered') {
-            if ( (Carbon::createFromTimestamp($this->auction->more_details['end_date']) >= Carbon::now()) && ($start_auction <= Carbon::now()) ) {
+            $end_date=Carbon::createFromTimestamp($this->start_date)->addSeconds($this->auction->duration);
+//            if ( (Carbon::createFromTimestamp($this->auction->more_details['end_date']) >= Carbon::now()) && ($start_auction <= Carbon::now()) ) {
+            if ( ($end_date >= Carbon::now()) && ($start_auction <= Carbon::now()) ) {
                 $arr['live'] = true;
                 $arr['status'] = 'live';
             }
