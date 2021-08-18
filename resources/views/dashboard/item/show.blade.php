71bc727a860e22ff9dc78fe8c7f1bb1a2a01e7db
sa3d01 <sa3dsalem01@gmail.com>
2021-08-19 01:05:16 +0200

    18-8

--- resources/views/dashboard/item/show.blade.php
+++ resources/views/dashboard/item/show.blade.php
@@ -12,6 +12,8 @@
         width: 100% !important;
     }
 </style>
+<link rel="stylesheet" href="{{asset('panel/dropify/dist/css/dropify.min.css')}}">
+
 @endsection
 @section('content')
     <div class="content-i">
@@ -299,6 +301,43 @@
     </div>
 @endsection
 @section('script')
+    <script src="{{asset('panel/dropify/dist/js/dropify.min.js')}}"></script>
+    <script>
+        $(document).ready(function () {
+            // Basic
+            $('.dropify').dropify();
+            // Translated
+            $('.dropify-fr').dropify({
+                messages: {
+                    default: 'Glissez-déposez un fichier ici ou cliquez',
+                    replace: 'Glissez-déposez un fichier ou cliquez pour remplacer',
+                    remove: 'Supprimer',
+                    error: 'Désolé, le fichier trop volumineux'
+                }
+            });
+            // Used events
+            var drEvent = $('#input-file-events').dropify();
+            drEvent.on('dropify.beforeClear', function (event, element) {
+                return confirm("Do you really want to delete \"" + element.file.name + "\" ?");
+            });
+            drEvent.on('dropify.afterClear', function (event, element) {
+                alert('File deleted');
+            });
+            drEvent.on('dropify.errors', function (event, element) {
+                console.log('Has Errors');
+            });
+            var drDestroy = $('#input-file-to-destroy').dropify();
+            drDestroy = drDestroy.data('dropify')
+            $('#toggleDropify').on('click', function (e) {
+                e.preventDefault();
+                if (drDestroy.isDropified()) {
+                    drDestroy.destroy();
+                } else {
+                    drDestroy.init();
+                }
+            })
+        });
+    </script>
     <script type="text/javascript">
         $(document).ready(function () {
             let files=JSON.parse($("#itemImages").attr('data-images'));
