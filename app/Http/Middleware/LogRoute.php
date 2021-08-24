9385483e3baa78368ba36fc782e86a8616d26af8
sa3d01 <sa3dsalem01@gmail.com>
2021-08-24 23:48:20 +0200

    18-8/wallet

--- app/Http/Middleware/LogRoute.php
+++ app/Http/Middleware/LogRoute.php
@@ -4,30 +4,56 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
+use Monolog\Logger;
+
 class LogRoute
 {
+
+    private $logger;
+
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        $response = $next($request);
+
+        $this->logger->info('Dump request', [
+            'request' => serialize($request),
+            'response' => serialize($response),
+        ]);
+
+        $log = [
+            'uri' => $request->getUri(),
+            'method' => $request->getMethod(),
+            'body' => json_encode($request->all()),
+            'response' => $response->getStatusCode(),
+            'ip' => $request->ip()
+        ];
+        DB::table('logs')->insert($log);
+
+        return $response;
     }
+
     public function terminate($request, $response)
     {
         $log = [
             'uri' => $request->getUri(),
             'method' => $request->getMethod(),
             'body' => json_encode($request->all()),
-            'response' =>$response->getStatusCode(),
+            'response' => $response->getStatusCode(),
             'ip' => $request->ip()
         ];
         DB::table('logs')->insert($log);
-//        Log::info('app.requests', ['request' => $request->all(), 'response' => $response]);
     }
+
 }
