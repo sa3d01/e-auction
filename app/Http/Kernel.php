817f48a77903d9d1ffb825670f968be01eb62536
sa3d01 <sa3dsalem01@gmail.com>
2021-08-24 22:25:07 +0200

    18-8/wallet

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\LogRoute;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -39,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-           // 'throttle:60,1',
+            'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
@@ -65,5 +66,6 @@ class Kernel extends HttpKernel
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+        'log.route' => LogRoute::class
     ];
 }
---
+++ app/Http/Middleware/LogRoute.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+class LogRoute
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response= $next($request);
+        $log = [
+            'uri' => $request->getUri(),
+            'method' => $request->getMethod(),
+            'body' => $request->all(),
+            'response' => $response->getContent(),
+            'ip' => $request->ip()
+        ];
+        DB::table('logs')->insert($log);
+        Log::info(json_encode($log));
+        return $response;
+    }
+}
