8dd97cd41e9551d6f080184ec33c92d4b2bd774b
sa3d01 <sa3dsalem01@gmail.com>
2022-02-14 23:38:46 +0200

    time

--- app/Http/Controllers/Api/MasterController.php
+++ app/Http/Controllers/Api/MasterController.php
@@ -114,8 +114,7 @@ class MasterController extends Controller
     {
         if ($auction_item->more_details != null) {
             $now=date('Y-m-d H:i:s',$bid_time);
-            $date = DateTime::createFromFormat('U', $bid_time);
-            return $this->sendError($date);
+            return $this->sendError(date('m/d/Y H:i:s', time()));
             $bid_pause_period=Setting::value('bid_pause_period');
             if ($auction_item->more_details['status'] == 'expired' || $auction_item->more_details['status'] == 'paid') {
                 $ar_msg='هذه المركبة قد انتهى وقت المزايدة عليها :(';
